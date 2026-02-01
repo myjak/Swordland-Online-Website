25df38999765998989a22a41e4b134631836637b
myjak <80157121+myjak@users.noreply.github.com>
2026-02-01 19:35:09 +0100

    removed gallery section import from main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ import { AboutSection } from "@/src/components/about-section";
 import { CommunitySection } from "@/src/components/community-section";
 import { Footer } from "@/src/components/footer";
 import { JoinSection } from "@/src/components/join-section";
-import { Analytics } from "@vercel/analytics/next"
-import { GallerySection } from "@/src/components/gallery-section"
+import { Analytics } from "@vercel/analytics/next";
+//import { GallerySection } from "@/src/components/gallery-section"
 
 export default function Page() {
   return (
